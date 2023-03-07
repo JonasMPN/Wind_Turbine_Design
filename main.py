48e50e92ed35c7813e95fc8d9b21cc496fb5dfaf
JonasMPN <102286850+JonasMPN@users.noreply.github.com>
2023-03-07 14:47:59 +0100

    Add interpolator

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 import numpy as np
+import pandas as pd
+import scipy.interpolate as interpolate
 
-tsr = 8
+
+class AirfoilInterpolator:
+    def __init__(self,
+                 dir_data: str,
+                 data_profiles: list or str,
+                 **pandas_read_csv):
+        if type(data_profiles) == str:
+            data_profiles = [data_profiles]
+        self.dir_data = dir_data
+        self.df_profiles = list()
+        for file in data_profiles:
+            self.df_profiles.append(pd.read_csv(dir_data + "/" + file, **pandas_read_csv))
+
+    def interpolate(self,
+                    to_interpolate: dict) -> None:
+        """
+        Multidimensional interpolation. The data is taken from the files that are specified during the initialisation of
+        the class' instance. The parameter 'to_interpolate' states which parameter(s) is(are) to be interpolated and on
+        which arguments this interpolation is based. The keys of 'to_interpolate' specify the parameters that will be
+        interpolated. The values of each key specify on which arguments the interpolation is based upon.
+        :param to_interpolate: keys: values to interpolate, values: arguments for interpolation
+        :return: None
+        """
+        for to_inter, arguments in to_interpolate.items():
+            if type(arguments) == str:
+                arguments = [arguments]
+            points = {var: list() for var in arguments}
+            values = list()
+            for df in self.df_profiles:
+                for _, row in df.iterrows():
+                    for arg in arguments:
+                        points[arg].append(row[arg])
+                    values.append(row[to_inter])
+            points = np.asarray([all_points for all_points in points.values()]).T
+            self.__dict__[to_inter] = interpolate.LinearNDInterpolator(points, values)
 
