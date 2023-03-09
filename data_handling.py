d7e08118ab4c2e6ec4520c5098f85bbc4d39bb98
Jonas <jonas.mpn@gmail.com>
2023-03-09 10:58:52 +0100

    Initial commit of AirfoilInterpolator

---
+++ data_handling.py
@@ -0,0 +1,61 @@
+import scipy.interpolate as interpolate
+import numpy as np
+import pandas as pd
+
+
+class AirfoilInterpolator:
+    def __init__(self,
+                 dir_data: str,
+                 data_profiles: list or str,
+                 **pandas_read_csv):
+        if type(data_profiles) == str:
+            data_profiles = [data_profiles]
+        self.df = pd.DataFrame()
+        for file in data_profiles:
+            self.df = pd.concat([self.df, pd.read_csv(dir_data+"/"+file, **pandas_read_csv)], ignore_index=True)
+        self.interpolator = dict()
+
+    def get_df_profiles(self):
+        return self.df
+
+    def prepare_interpolation(self,
+                             to_interpolate: dict,
+                             constraints: dict = None) -> None:
+        """
+        Multidimensional interpolation. The data is taken from the files that are specified during the initialisation of
+        the class' instance. The parameter 'to_interpolate' states which parameter(s) is(are) to be interpolated and on
+        which arguments this interpolation is based. The keys of 'to_interpolate' specify the parameters that will be
+        interpolated. The values of each key specify on which arguments the interpolation is based upon.
+        :param to_interpolate: keys: values to interpolate, values: arguments for interpolation
+        :param constraints: dict of dicts. First key is parameter to be interpolated, value of that key is again a
+        dict. That dict has the arguments for the interpolation as keys and the ranges that should be used as value (
+        as tuple)
+        :return: None
+        """
+        constraints = constraints if type(constraints) == dict else {}
+        for to_inter, arguments in to_interpolate.items():
+            if type(arguments) == str:
+                arguments = [arguments]
+            points = {var: list() for var in arguments}
+            values = list()
+            df_data_points = self.df
+
+            if to_inter in constraints.keys():
+                df_data_points = pd.DataFrame(columns=df_data_points.columns.tolist())
+                for arg_parameter, constraint_values in constraints[to_inter].items():
+                    for constraint_value in constraint_values:
+                        df_data_points = pd.concat([df_data_points,
+                                                   self.df[self.df[arg_parameter]==constraint_value]],
+                                                   ignore_index=True)
+            for _, row in df_data_points.iterrows():
+                for arg in arguments:
+                    points[arg].append(row[arg])
+                values.append(row[to_inter])
+            points = np.asarray([all_points for all_points in points.values()]).T
+            if points.shape[1] == 1:
+                self.interpolator[to_inter] = interpolate.interp1d(points.flatten(), values)
+            else:
+                self.interpolator[to_inter] = interpolate.LinearNDInterpolator(points, values)
+
+    def __getitem__(self, item):
+        return self.interpolator[item]
